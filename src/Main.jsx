a4401cdddb3e5bf5aab29a5ae16682728de8c4d1
HosSamNeo <81299330+HosSamNeo@users.noreply.github.com>
2022-12-13 00:10:02 +0200

    Header Component
    Header Component

---
+++ src/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+// import Landing from './Landing';
+import './main.css';
+const Main = () => {
+return (
+        <section className='main'>
+                <div className="container">
+                    <Header />
+                    {/* <Landing /> */}
+                </div>
+        </section>
+)
+}
+
+export default Main;
